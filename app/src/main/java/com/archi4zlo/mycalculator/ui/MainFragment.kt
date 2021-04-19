584dc92d10b04e934d8b8c2170699042f610f8ca
Anton Sych <anton.sych.job@gmail.com>
2021-04-20 00:11:50 +0300

    Add cursor animation

--- app/src/main/java/com/archi4zlo/mycalculator/ui/MainFragment.kt
+++ app/src/main/java/com/archi4zlo/mycalculator/ui/MainFragment.kt
@@ -6,7 +6,14 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.archi4zlo.mycalculator.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import android.view.animation.Animation
+
+import android.view.animation.AlphaAnimation
+
+
+
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
@@ -18,8 +25,15 @@ class MainFragment constructor(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
+        inputAnimation()
         Log.d(TAG, "onViewCreated: ${welcomeString}")
+    }
+    fun inputAnimation(){
+        val anim: Animation = AlphaAnimation(0.0f, 1.0f)
+        anim.duration = 300 //You can manage the time of the blink with this parameter
+        anim.startOffset = 20
+        anim.repeatMode = Animation.REVERSE
+        anim.repeatCount = Animation.INFINITE
+        TVInputNumbers.startAnimation(anim)
     }
 }
